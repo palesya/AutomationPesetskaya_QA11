41549270a3f6f66945b390a6b2c6768d93df179c
”Alesya <palesyav89@gmail.com>
2022-02-03 16:59:41 +0300

    homework 7 is ready

--- src/test/java/Homework_7/Homework_7.java
+++ src/test/java/Homework_7/Homework_7.java
@@ -1,4 +1,30 @@
 package Homework_7;
 
-public class Homework_7 {
+import BaseObjects.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework_7 extends BaseTest {
+
+    @Test(priority = 1)
+    public void checkboxesAreBothSelected() {
+        driver.get("http://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("//a[@href='/checkboxes']")).click();
+        WebElement checkbox1 = driver.findElement(By.xpath("//input[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//input[2]"));
+        checkbox1.click();
+        Assert.assertTrue(checkbox1.isSelected());
+        Assert.assertTrue(checkbox2.isSelected());
+    }
+
+    @Test(priority = 2)
+    public void checkOption1InDropdown() {
+        driver.get("http://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("//a[@href='/dropdown']")).click();
+        String values = driver.findElement(By.id("dropdown")).getText();
+        String[] allValuesArray = values.split("\\r?\\n");
+        Assert.assertEquals(allValuesArray[1].trim(), "Option 1");
+    }
 }
