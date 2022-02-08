8b3f444d8be2f720030d125a44466b377ccc09c2
”Alesya <palesyav89@gmail.com>
2022-02-08 11:39:59 +0300

    Lecture 7 added

--- src/main/java/BaseObjects/BaseTest.java
+++ src/main/java/BaseObjects/BaseTest.java
@@ -1,24 +1,27 @@
 package BaseObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import static BaseObjects.DriverCreation.closeDriver;
 import static BaseObjects.DriverCreation.getDriver;
-import static BaseObjects.DriverCreation.Drivers.*;
+import static org.openqa.selenium.remote.BrowserType.CHROME;
 
 public class BaseTest {
     protected WebDriver driver;
+    protected ITestContext context;
 
     @BeforeTest
-    public void precondition() {
+    public void precondition(ITestContext context) {
+        this.context = context;
         this.driver = getDriver(CHROME);
     }
 
     @AfterTest
     public void postcondition() {
-        closeDriver();
+        closeDriver(CHROME);
     }
 
 }
