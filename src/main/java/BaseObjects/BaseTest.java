a309474daac5055aeba070601aa3eb5f02677f9f
”Alesya <palesyav89@gmail.com>
2022-02-09 19:48:32 +0300

    Map example added

--- src/main/java/BaseObjects/BaseTest.java
+++ src/main/java/BaseObjects/BaseTest.java
@@ -12,16 +12,18 @@ import static org.openqa.selenium.remote.BrowserType.CHROME;
 public class BaseTest {
     protected WebDriver driver;
     protected ITestContext context;
+    private String browserName;
 
     @BeforeTest
     public void precondition(ITestContext context) {
         this.context = context;
-        this.driver = getDriver(CHROME);
+        this.browserName = context.getSuite().getParameter("browser") == null ? System.getProperty("browser") : context.getSuite().getParameter("browser");
+        this.driver = getDriver(browserName == null ? "CHROME" : browserName);
     }
 
     @AfterTest
     public void postcondition() {
-        closeDriver(CHROME);
+        closeDriver(browserName == null ? "CHROME" : browserName);
     }
 
 }
--- src/test/java/Homework_8/Homework_8.java
+++ src/test/java/Homework_8/Homework_8.java
@@ -1,14 +1,17 @@
 package Homework_8;
 
 import BaseObjects.BaseTest;
+import com.google.common.io.ByteSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Homework_8 extends BaseTest {
 
@@ -16,12 +19,21 @@ public class Homework_8 extends BaseTest {
     public void checkFirstValueInList() {
         driver.get("D:/AutomationPesetskaya_QA11/src/test/java/Homework_8/task_8.html");
         //Создать коллекцию из значений в первой колонке
-        List<WebElement> brandsWebEl = driver.findElements(By.cssSelector("td:first-child"));
-        List<String> brandsStringList = new ArrayList<>() {{
-            brandsWebEl.forEach((element) -> add(element.getText()));
-        }};
+        List<String> brandsStringList = driver.findElements(By.cssSelector("td:first-child")).stream().map(el -> el.getText()).collect(Collectors.toList());
         Assert.assertEquals(brandsStringList.get(0), "Renault");
     }
+
+    @Test(priority = 1)
+    public void createCollectionMap() {
+        List<String> brandsStringList = driver.findElements(By.tagName("th")).stream().map(el -> el.getText()).collect(Collectors.toList());
+        Map<String, List<String>> tableData = new HashMap<>();
+        for (int i = 0; i < brandsStringList.size(); i++) {
+            List<String> cell = driver.findElements(By.xpath("//tr/td[" + (i + 1) + "]")).stream().map(el -> el.getText()).collect(Collectors.toList());
+        tableData.put(brandsStringList.get(i),cell);
+        }
+        System.out.println(tableData.get("Brand").get(0));
+    }
+
 
     @Test(priority = 2)
     public void checkElementsOnPage() {
@@ -40,10 +52,10 @@ public class Homework_8 extends BaseTest {
         //Проверить что картинка существует
         Assert.assertTrue(driver.findElement(By.cssSelector("[alt^='R']")).isEnabled());
         //Проверить текст тега р
-        Assert.assertEquals(driver.findElement(By.xpath("//p[2]")).getText(),"- Ferdinand Porsche");
+        Assert.assertEquals(driver.findElement(By.xpath("//p[2]")).getText(), "- Ferdinand Porsche");
         //Кликнуть на ссылку и проверить что новая страница была открыта
         driver.findElement(By.tagName("a")).click();
-        Assert.assertEquals(driver.getCurrentUrl(),"https://www.renault.by/cars/koleos/overview.html");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.renault.by/cars/koleos/overview.html");
     }
 }
 
