b42ad39bdf16d42cb76a4d0d26120b0f67b29af1
”Alesya <palesyav89@gmail.com>
2022-05-04 14:18:03 +0300

    Driver Manager removed from Base Test to Listener

--- src/main/java/TestNGUtils/Listener.java
+++ src/main/java/TestNGUtils/Listener.java
@@ -1,6 +1,10 @@
 package TestNGUtils;
 
+import Driver.DriverManager;
+import Driver.DriverManagerFactory;
 import Properties.PropertyReader;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 
@@ -8,9 +12,13 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Properties;
 
+import static Properties.PropertyReader.getProperties;
+
 public class Listener implements ITestListener {
     private static Properties properties;
     private static ITestContext context;
+    private WebDriver driver;
+    private DriverManager driverManager;
 
     @Override
     public void onStart(ITestContext context) {
@@ -18,10 +26,17 @@ public class Listener implements ITestListener {
         System.setProperty("logger_time",getSimpleDate());
         new PropertyReader(context.getSuite().getParameter("config") == null ? System.getProperty("config") : context.getSuite().getParameter("config"));
         this.properties = PropertyReader.getProperties();
+        this.driverManager= DriverManagerFactory.getManager(DriverManagerType.valueOf(getProperties().getProperty("browser")));
+        this.driver = DriverManager.getDriver();
     }
 
     private String getSimpleDate() {
         return new SimpleDateFormat("yyyyMMdd-HHmmss").format(Calendar.getInstance().getTime());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        DriverManager.closeDriver();
     }
 
 }
