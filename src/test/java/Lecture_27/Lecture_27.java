d9470f7eec9db9ce1f9e8333fefef5e6c264d3c8
”Alesya <palesyav89@gmail.com>
2022-05-05 16:19:55 +0300

    Yaml added

--- src/test/java/Lecture_27/Lecture_27.java
+++ src/test/java/Lecture_27/Lecture_27.java
@@ -1,4 +1,16 @@
 package Lecture_27;
 
+import Yaml.YamlReader;
+import org.testng.annotations.Test;
+
 public class Lecture_27 {
+
+    @Test
+    public void test() {
+        YamlReader yamlReader = new YamlReader();
+        System.out.println(yamlReader.readYaml("config"));
+
+        Person person = (Person) yamlReader.readYaml(Person.class, "config");
+        System.out.println(person.toString());
+    }
 }
--- src/test/java/Lecture_27/Person.java
+++ src/test/java/Lecture_27/Person.java
@@ -1,4 +1,30 @@
 package Lecture_27;
 
 public class Person {
+    private String name;
+    private String lastname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Person with " +
+                "name " + name +
+                " and lastname " + lastname;
+    }
 }
+
